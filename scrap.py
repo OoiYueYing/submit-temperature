46b45e26bc795a5f3146ec2c8d3f8e73a4d8a26f
yueying <ooiyueying@gmail.com>
2020-03-21 22:37:13 +0800

    added code inorder to use selenium

--- scrap.py
+++ scrap.py
@@ -4,6 +4,19 @@ import time
 from selenium import webdriver
 import yaml
 
+# this is added to deploy onto heroku
+import os
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+# Now you can start using Selenium
+
+
 conf = yaml.load(open('./credentials.yml'))
 username = conf['ntu']['userName']
 pwd = conf['ntu']['password']
